6b9d766afaf559c4f21447221caf3785b2fc8517
DanielGord0n <gordondan2@gmail.com>
2025-11-26 18:23:20 -0500

    vulnerabilities fixed

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa';
 import ProjectCard3D from '../components/ProjectCard3D';
 import PageTransition from '../components/PageTransition';
